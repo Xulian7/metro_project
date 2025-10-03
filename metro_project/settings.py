f413ba929e4c4d662b1ce38df144ace8da2957ea
Xulian7 <jfgrossop@outlook.com>
2025-10-02 20:17:11 -0500

    Revision static/

--- metro_project/settings.py
+++ metro_project/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 # Archivos estáticos para Railway
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
