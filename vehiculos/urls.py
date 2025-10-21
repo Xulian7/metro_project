919a5ab9fef734d9223e89d6da759416776eaf87
Xulian7 <jfgrossop@outlook.com>
2025-10-20 22:56:19 -0500

    Actualiza módulo clientes (editar con AJAX)

--- vehiculos/urls.py
+++ vehiculos/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.vehiculos_dashboard, name='vehiculos_dashboard'),
-    path('vehiculos/update/', views.vehiculo_update, name='vehiculo_update'),
+    path('update/', views.vehiculo_update, name='vehiculo_update'),
+    
 
 ]
 
