b67ef7c458351578f5e0814fcec52122ba08c4f7
Xulian7 <jfgrossop@outlook.com>
2025-10-26 17:12:21 -0500

    Almacen carga masiva

--- almacen/urls.py
+++ almacen/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.almacen_dashboard, name='almacen_dashboard'),
+    path("productos/editar/<int:id>/", views.editar_producto, name="editar_producto"),
 ]
