4243448579bb6033493e64ca105a49a0514cc299
Xulian7 <jfgrossop@outlook.com>
2025-11-25 23:02:23 -0500

    App Clientes

--- terminal_pagos/urls.py
+++ terminal_pagos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import terminal_pagos_view, get_datos_vehiculo
 
 urlpatterns = [
-    path('pagos/', views.terminal_pagos_view, name='terminal_pagos'),
+    path('', terminal_pagos_view, name="terminal_pagos"),
+    path('get-datos', get_datos_vehiculo, name="get_datos_vehiculo"),
 ]
