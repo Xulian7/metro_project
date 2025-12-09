fcb6d97a2ca2e6557d41325f60b23ba23ac32862
Xulian7 <jfgrossop@outlook.com>
2025-12-08 20:10:17 -0500

    Widget Vitrina

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,14 @@
+# urls.py
 from django.urls import path
-from .views import MyLoginView, MyLogoutView, home
+from .views import MyLoginView, MyLogoutView, home, vehiculos_vitrina
 
 urlpatterns = [
-    path('', home, name='home'),
+    # Página principal con slider de vehículos en vitrina
+    path('', vehiculos_vitrina, name='home'),
+
+    # Login / Logout
     path('login/', MyLoginView.as_view(), name='login'),
     path('logout/', MyLogoutView.as_view(), name='logout'),
+
+
 ]
