490426014e9c75dc57fca513799ef4838a1c20c2
Xulian7 <jfgrossop@outlook.com>
2025-12-08 19:53:56 -0500

    Widget Vitrina

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from vehiculos.models import Vehiculo
 
 class MyLoginView(LoginView):
     template_name = 'accounts/login.html'
@@ -12,3 +13,7 @@ class MyLogoutView(LogoutView):
 def home(request):
     return render(request, 'accounts/home.html', {'user': request.user})
 
+
+def vehiculos_vitrina(request):
+    vehiculos = Vehiculo.objects.filter(estado='Vitrina')
+    return render(request, 'vehiculos_slider.html', {'vehiculos': vehiculos})
