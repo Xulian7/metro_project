044f5a7b25d06d1a4d27477cbeb2f69c6f55b08b
Xulian7 <jfgrossop@outlook.com>
2026-01-01 10:57:31 -0500

    App Terminal

--- terminal_pagos/forms.py
+++ terminal_pagos/forms.py
@@ -3,6 +3,20 @@ from django.forms import inlineformset_factory
 from .models import Factura, ItemFactura
 from arrendamientos.models import Contrato
 
+class ContratoSelect(forms.Select):
+    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
+        option = super().create_option(
+            name, value, label, selected, index, subindex=subindex, attrs=attrs
+        )
+
+        if value:
+            try:
+                contrato = Contrato.objects.get(pk=value)
+                option["attrs"]["data-tarifa"] = contrato.tarifa
+            except Contrato.DoesNotExist:
+                pass
+
+        return option
 
 
 class FacturaForm(forms.ModelForm):
@@ -40,17 +54,3 @@ ItemFacturaFormSet = inlineformset_factory(
 )
 
 
-class ContratoSelect(forms.Select):
-    def create_option(self, name, value, label, selected, index, subindex=None, attrs=None):
-        option = super().create_option(
-            name, value, label, selected, index, subindex=subindex, attrs=attrs
-        )
-
-        if value:
-            try:
-                contrato = Contrato.objects.get(pk=value)
-                option["attrs"]["data-tarifa"] = contrato.tarifa
-            except Contrato.DoesNotExist:
-                pass
-
-        return option
