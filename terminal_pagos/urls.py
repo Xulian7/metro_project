3035b3d2fc98c99bac27388e68216f53b5c7ff07
Xulian7 <jfgrossop@outlook.com>
2026-01-18 13:30:44 -0500

    Record medios

--- terminal_pagos/urls.py
+++ terminal_pagos/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("facturar/", views.crear_factura, name="crear_factura"),
     path("catalogos-pago/", views.catalogos_pago, name="catalogos_pago"),
     path("medios-pago/", views.medios_pago, name="medios_pago"),
-    path("validar-medio/<int:config_id>/", views.validar_medio_pago, name="validar_medio_pago"),
+    path("validar-medio/<int:config_id>/", views.validar_pago, name="validar_medio_pago"),
     
 ]
