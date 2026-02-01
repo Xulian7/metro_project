c036f836c7d26be27dcd87eb4cf09cd75d828b5d
Xulian7 <jfgrossop@outlook.com>
2026-01-31 22:37:27 -0500

    Cancelar creditos

--- creditos/urls.py
+++ creditos/urls.py
@@ -6,12 +6,8 @@ app_name = "creditos"
 urlpatterns = [
     path("nuevo/", views.crear_credito, name="crear_credito"), # type: ignore
 
-    path(
-        "items/<int:credito_id>/",
-        views.popover_items_credito,
-        name="popover_items_credito"
-    ),
+    path("items/<int:credito_id>/", views.popover_items_credito, name="popover_items_credito"), # type: ignore
     
-    path("creditos/cancelar/<int:credito_id>/", views.cancelar_credito, name="cancelar_credito"),
+    path("cancelar/<int:credito_id>/", views.cancelar_credito, name="cancelar_credito"),
 
 ]
