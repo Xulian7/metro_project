d5e5468f69337a280ab2df0138a54b937f42cb96
Xulian7 <jfgrossop@outlook.com>
2026-02-01 20:49:31 -0500

    Reportes afectados

--- terminal_pagos/admin.py
+++ terminal_pagos/admin.py
@@ -1,22 +1,3 @@
 from django.contrib import admin
-from .models import Factura, ItemFactura, PagoFactura
 
-
-class ItemFacturaInline(admin.TabularInline):
-    model = ItemFactura
-    extra = 0
-
-
-class PagoFacturaInline(admin.TabularInline):
-    model = PagoFactura
-    extra = 0
-
-
-@admin.register(Factura)
-class FacturaAdmin(admin.ModelAdmin):
-    inlines = [ItemFacturaInline, PagoFacturaInline]
-    list_display = ("id", "fecha", "estado", "total")
-
-
-admin.site.register(ItemFactura)
-admin.site.register(PagoFactura)
+# Register your models here.
